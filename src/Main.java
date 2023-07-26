be14801373924d8dd0e1a1184f1a335c6f726417
Apach <pavel_andreev@inbox.ru>
2023-07-26 17:02:03 +0400

    Bonusmiles

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 15_000;
+
+        int bonus = price / 20;
+
+        System.out.println("Бонус " + bonus);
+
+
+    }
+}
